a543c6160e53093f503563d113a2fe18e1d7cc1a
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-12-28 20:52:00 +0530

    updated for deployment error

--- client/app/(auth)/sign-in/page.tsx
+++ client/app/(auth)/sign-in/page.tsx
@@ -1,28 +1,26 @@
 "use client"
-import { useEffect } from "react"
+import { useEffect, Suspense } from "react"
 import { LoginForm } from '@/components/login-form'
 import { Spinner } from '@/components/ui/spinner';
 import { authClient } from '@/lib/auth-client';
 import { useRouter, useSearchParams } from 'next/navigation';
 import React from 'react'
 
-const Page = () => {
+const SignInContent = () => {
   const { data, isPending } = authClient.useSession();
   const router = useRouter();
   const searchParams = useSearchParams();
 
   useEffect(() => {
     if (!isPending && data?.session && data?.user) {
-
       const callbackUrl = searchParams.get("callbackUrl")
       if (callbackUrl) {
-
         router.push(decodeURIComponent(callbackUrl))
       } else {
         router.push("/")
       }
     }
-  }, [data, isPending, router, searchParams])
+  }, [data, isPending, router, searchParams]);
 
   if (isPending) {
     return (
@@ -32,8 +30,18 @@ const Page = () => {
     )
   }
 
+  return <LoginForm />
+}
+
+const Page = () => {
   return (
-    <LoginForm />
+    <Suspense fallback={
+      <div className="flex flex-col items-center justify-center h-screen">
+        <Spinner />
+      </div>
+    }>
+      <SignInContent />
+    </Suspense>
   )
 }
 
